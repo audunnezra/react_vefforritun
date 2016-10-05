5d08470fdbf244cbdbf8523a54965534603f70de
Auðunn Ezra Kristinsson Simpson <FS453488@fss.is>
2016-10-05 10:05:41 +0000

    commit 4

--- components/CommentBox.js
+++ components/CommentBox.js
@@ -7,9 +7,9 @@ var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
-      <h1>Comments</h1>
-      <CommentList />
-      <CommentForm />
+        <h1>Comments</h1>
+        <CommentList data={this.props.data} />
+        <CommentForm />
       </div>
     );
   }
